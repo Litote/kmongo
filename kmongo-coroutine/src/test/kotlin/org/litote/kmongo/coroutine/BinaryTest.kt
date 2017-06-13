338f99a416b192e5222d61b128703e68fde3fcfc
Deny Prasetyo <jasoet87@gmail.com>
2017-06-14 06:29:54 +0700

    Add BinaryTest for coroutine

---
+++ kmongo-coroutine/src/test/kotlin/org/litote/kmongo/coroutine/BinaryTest.kt
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2016 Litote
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.litote.kmongo.coroutine
+
+import kotlinx.coroutines.experimental.runBlocking
+import org.bson.types.Binary
+import org.junit.Assert.assertArrayEquals
+import org.junit.Before
+import org.junit.Test
+import org.litote.kmongo.MongoOperator.binary
+import org.litote.kmongo.MongoOperator.type
+import org.litote.kmongo.coroutine.BinaryTest.BinaryFriend
+import kotlin.reflect.KClass
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
+
+class BinaryTest : KMongoAsyncBaseTest<BinaryFriend>() {
+
+    data class BinaryFriend(val _id: Binary, var name: String = "none")
+
+    lateinit var friendId: Binary
+
+    override fun getDefaultCollectionClass(): KClass<BinaryFriend> = BinaryFriend::class
+
+    @Before
+    fun setup() {
+        friendId = Binary("kmongo".toByteArray())
+    }
+
+    @Test
+    fun testUpdate() = runBlocking {
+        val expectedId = Binary("friend2".toByteArray())
+        val expected = BinaryFriend(expectedId, "friend2")
+
+        singleResult<Void> { col.insertOne(expected, it) }
+        expected.name = "new friend"
+
+        col.updateOne("{_id:${expectedId.json}}", expected)
+        val savedFriend = col.findOne("{_id:${expectedId.json}}")
+        assertNotNull(savedFriend)
+        assertEquals(expected, savedFriend)
+    }
+
+    @Test
+    fun testInsert() = runBlocking {
+        val expectedId = Binary("friend".toByteArray())
+        val expected = BinaryFriend(expectedId, "friend")
+
+        singleResult<Void> { col.insertOne(expected, it) }
+        val savedFriend = col.findOne("{_id:${expectedId.json}}")
+        assertNotNull(savedFriend)
+        assertEquals(expected, savedFriend)
+    }
+
+    @Test
+    fun testRemove() = runBlocking {
+        col.deleteOne("{_id:${friendId.json}}")
+        val shouldNull = col.findOne("{_id:${friendId.json}}")
+        assertNull(shouldNull)
+    }
+
+    @Test
+    fun canMarshallBinary() = runBlocking {
+        val doc = BinaryFriend(Binary("abcde".toByteArray()))
+
+        singleResult<Void> { col.insertOne(doc, it) }
+        val count = col.count("{'_id' : { $binary : 'YWJjZGU=' , $type : '0'}}")
+        val savedDoc = col.findOne("{_id:${doc._id.json}}") ?: throw IllegalStateException("Must not NUll")
+
+        assertEquals(1, count)
+        assertEquals(doc._id.type, savedDoc._id.type)
+        assertArrayEquals(doc._id.data, savedDoc._id.data)
+    }
+
+}
+
