a8fb0e1398ff83ed3dad8ee89eb2a1f74c114f49
Dilius <diliuskh@gmail.com>
2018-03-27 00:13:51 +0300

    resolves #22 add rxjava api

---
+++ kmongo-flapdoodle/src/main/kotlin/org/litote/kmongo/rxjava2/RxFlapdoodleRule.kt
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2016 Litote
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.litote.kmongo.rxjava2
+
+import com.mongodb.async.SingleResultCallback
+import com.mongodb.async.client.MongoClient
+import com.mongodb.async.client.MongoCollection
+import com.mongodb.async.client.MongoDatabase
+import io.reactivex.Completable
+import io.reactivex.Maybe
+import org.bson.types.ObjectId
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+import org.litote.kmongo.async.AsyncTestClient
+import org.litote.kmongo.util.KMongoUtil
+import kotlin.reflect.KClass
+
+/**
+ * A [org.junit.Rule] to help writing tests for KMongo using [Flapdoodle](http://flapdoodle-oss.github.io/de.flapdoodle.embed.mongo/).
+ */
+class RxFlapdoodleRule<T : Any>(val defaultDocumentClass: KClass<T>,
+                                val generateRandomCollectionName: Boolean = false,
+                                val dbName: String = "test") : TestRule {
+
+    companion object {
+
+        inline fun <reified T : Any> rule(generateRandomCollectionName: Boolean = false): RxFlapdoodleRule<T> = RxFlapdoodleRule(T::class, generateRandomCollectionName)
+
+    }
+
+    val mongoClient: MongoClient = AsyncTestClient.instance
+    val database: MongoDatabase by lazy {
+        mongoClient.getDatabase(dbName)
+    }
+
+    private inline fun <T> maybeResult(crossinline callback: (SingleResultCallback<T>) -> Unit): Maybe<T> {
+        return Maybe.create { emitter ->
+            callback(SingleResultCallback { result: T?, throwable: Throwable? ->
+                when {
+                    throwable != null -> emitter.onError(throwable)
+                    result != null -> emitter.onSuccess(result)
+                    else -> emitter.onComplete()
+                }
+            })
+        }
+    }
+
+    private inline fun completableResult(crossinline callback: (SingleResultCallback<Void>) -> Unit): Completable {
+        return Completable.create { emitter ->
+            callback(SingleResultCallback { result: Void?, throwable: Throwable? ->
+                when {
+                    throwable != null -> emitter.onError(throwable)
+                    else -> emitter.onComplete()
+                }
+            })
+        }
+    }
+
+    inline fun <reified T : Any> getCollection(): MongoCollection<T> = database.getCollection(KMongoUtil.defaultCollectionName(T::class), T::class.java)
+
+    fun <T : Any> getCollection(clazz: KClass<T>): MongoCollection<T> = database.getCollection(KMongoUtil.defaultCollectionName(clazz), clazz.java)
+
+    fun <T : Any> getCollection(name: String, clazz: KClass<T>): MongoCollection<T> = database.getCollection(name, clazz.java)
+
+    inline fun <reified T : Any> dropCollection() = dropCollection(KMongoUtil.defaultCollectionName(T::class))
+
+    fun dropCollection(clazz: KClass<*>) = dropCollection(KMongoUtil.defaultCollectionName(clazz))
+
+    fun dropCollection(collectionName: String) = database.getCollection(collectionName).drop()
+
+    fun <T> MongoCollection<T>.drop(): Completable {
+        return completableResult { this.drop(it) }
+    }
+
+    val col: MongoCollection<T> by lazy {
+        val name = if (generateRandomCollectionName) {
+            ObjectId().toString()
+        } else {
+            KMongoUtil.defaultCollectionName(defaultDocumentClass)
+        }
+
+        getCollection(name, defaultDocumentClass)
+    }
+
+    override fun apply(base: Statement, description: Description): Statement {
+        return object : Statement() {
+
+            override fun evaluate() {
+                try {
+                    base.evaluate()
+                } finally {
+                    col.drop().blockingAwait()
+                }
+            }
+        }
+    }
+
+}
---
+++ kmongo-rxjava2-core-tests/src/main/kotlin/org/litote/kmongo/rxjava2/FindOneTest.kt
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2016 Litote
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.litote.kmongo.rxjava2
+
+import com.mongodb.ReadPreference
+import com.mongodb.client.model.Filters
+import org.bson.types.ObjectId
+import org.junit.Test
+import org.litote.kmongo.MongoOperator.oid
+import org.litote.kmongo.model.Friend
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+
+/**
+ *
+ */
+class FindOneTest : KMongoRxBaseTest<Friend>() {
+
+    @Test
+    fun canFindOne() {
+        col.insertOne(Friend("John", "22 Wall Street Avenue")).blockingAwait()
+        val friend = col.findOne("{name:'John'}").blockingGet() ?: throw AssertionError("Value must not null!")
+        assertEquals("John", friend.name)
+    }
+
+    @Test
+    fun canFindOneBson() {
+        col.insertOne(Friend("John", "22 Wall Street Avenue")).blockingAwait()
+        val friend = col.findOne(Filters.eq("name", "John")).blockingGet() ?: throw AssertionError("Value must not null!")
+        assertEquals("John", friend.name)
+    }
+
+    @Test
+    fun canFindOneWithEmptyQuery() {
+        col.insertOne(Friend("John", "22 Wall Street Avenue")).blockingAwait()
+        val friend = col.findOne().blockingGet() ?: throw AssertionError("Value must not null!")
+        assertEquals("John", friend.name)
+    }
+
+    @Test
+    fun canFindOneWithObjectId() {
+        val john = Friend(ObjectId(), "John")
+        col.insertOne(john).blockingAwait()
+        val friend = col.findOneById(john._id ?: Any()).blockingGet() ?: throw AssertionError("Value must not null!")
+        assertEquals(john._id, friend._id)
+    }
+
+    @Test
+    fun canFindOneWithObjectIdInQuery() {
+        val id = ObjectId()
+        val john = Friend(id, "John")
+        col.insertOne(john).blockingAwait()
+        val friend = col.findOne("{_id:${id.json}}").blockingGet() ?: throw AssertionError("Value must not null!")
+        assertEquals(id, friend._id)
+    }
+
+    @Test
+    fun canFindOneWithObjectIdAsString() {
+        val id = ObjectId()
+        val john = Friend(id, "John")
+        col.insertOne(john).blockingAwait()
+        val friend = col.findOne("{_id:{$oid:'$id'}}").blockingGet() ?: throw AssertionError("Value must not null!")
+        assertEquals(id, friend._id)
+    }
+
+    @Test
+    fun whenNoResultShouldReturnNull() {
+        val friend = col.findOne("{_id:'invalid-id'}").blockingGet()
+        assertNull(friend)
+    }
+
+    @Test
+    fun canFindOneWithReadPreference() {
+        col.insertOne(Friend("John", "22 Wall Street Avenue")).blockingAwait()
+        val friend = col.withReadPreference(ReadPreference.primaryPreferred()).findOne("{name:'John'}").blockingGet()
+                ?: throw AssertionError("Value must not null!")
+        assertEquals("John", friend.name)
+    }
+}
