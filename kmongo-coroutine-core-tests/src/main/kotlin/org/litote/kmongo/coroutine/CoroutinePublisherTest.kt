2531d376e7f57a1b5abe5218a8810b1a97c034a0
Julien Buret <jburet@gmail.com>
2020-05-20 22:56:56 +0200

    fixes #201 unit tests

--- kmongo-coroutine-core-tests/src/main/kotlin/org/litote/kmongo/coroutine/CoroutinePublisherTest.kt
+++ kmongo-coroutine-core-tests/src/main/kotlin/org/litote/kmongo/coroutine/CoroutinePublisherTest.kt
@@ -1,12 +1,7 @@
 package org.litote.kmongo.coroutine
 
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.verify
-import com.nhaarman.mockitokotlin2.whenever
 import junit.framework.Assert.assertEquals
-import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.toList
-import kotlinx.coroutines.reactive.asFlow
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
 import org.junit.experimental.categories.Category
@@ -16,26 +11,20 @@ import org.reactivestreams.Publisher
 @Category(CoreCategory::class)
 class CoroutinePublisherTest {
 
-    val publisher: Publisher<*> = mock()
-    val coroutine = publisher.coroutine
+    private val publisher: Publisher<Int> = DoSinglePublisher(1)
+    private val coroutine = publisher.coroutine
 
     @Test
     fun toList() {
         runBlocking {
-            val expected = listOf(1)
-            whenever(publisher.toList()).thenReturn(expected)
-            assertEquals(expected, coroutine.toList())
-            verify(publisher).toList()
+            assertEquals(listOf(1), coroutine.toList())
         }
     }
 
     @Test
     fun toFlow() {
         runBlocking {
-            val expected = listOf(1).asFlow()
-            whenever(publisher.asFlow()).thenReturn(expected)
-            assertEquals(expected.toList(), coroutine.toFlow().toList())
-            verify(publisher).asFlow()
+            assertEquals(listOf(1), coroutine.toFlow().toList())
         }
     }
 }
