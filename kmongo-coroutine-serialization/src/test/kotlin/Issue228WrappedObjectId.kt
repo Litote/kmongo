eea20053801fd737fa75876a5e070114bb60e78a
Julien Buret <jburet@gmail.com>
2020-10-11 21:47:05 +0200

    test #228

---
+++ kmongo-coroutine-serialization/src/test/kotlin/Issue228WrappedObjectId.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2016/2020 Litote
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import org.junit.Test
+import org.litote.kmongo.Id
+import org.litote.kmongo.coroutine.KMongoReactiveStreamsCoroutineBaseTest
+import org.litote.kmongo.id.MongoId
+import org.litote.kmongo.newId
+import kotlin.test.assertEquals
+
+@Serializable
+data class Tag(
+    @Contextual
+    @SerialName("_id") @MongoId val id: Id<Tag>? = newId(),
+    val displayName: String,
+    val description: String? = null,
+    val createdAt: Long = System.currentTimeMillis()
+)
+
+/**
+ *
+ */
+class Issue228WrappedObject : KMongoReactiveStreamsCoroutineBaseTest<Tag>() {
+
+    @Test
+    fun testSaveAndLoad() = runBlocking {
+        val tag = Tag(displayName = "foobar")
+        col.insertOne(tag)
+        assertEquals(tag, col.findOne())
+    }
+
+}
