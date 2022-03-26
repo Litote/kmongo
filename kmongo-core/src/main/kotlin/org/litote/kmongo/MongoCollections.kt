09a9540e9d8ff0ea82315c67483a759c1ac2b8b2
pauls <paul.souteyrat@timeplanner-app.com>
2022-03-26 12:59:34 +0100

    Adding ability to use setTo in updateOneById

--- kmongo-core/src/main/kotlin/org/litote/kmongo/MongoCollections.kt
+++ kmongo-core/src/main/kotlin/org/litote/kmongo/MongoCollections.kt
@@ -528,6 +528,25 @@ fun <T> MongoCollection<T>.updateOneById(
     updateOne(KMongoUtil.idFilterQuery(id), KMongoUtil.toBsonModifier(update, updateOnlyNotNullProperties), options)
 
 /**
+ * Update a single document in the collection according to the specified arguments.
+ *
+ * @param id        the object id
+ * @param updates   the setTo describing the updates
+ * @param options  the options to apply to the update operation
+ *
+ * @return the result of the update one operation
+ *
+ * @throws com.mongodb.MongoWriteException        if the write failed due some other failure specific to the update command
+ * @throws com.mongodb.MongoWriteConcernException if the write failed due being unable to fulfil the write concern
+ * @throws com.mongodb.MongoException             if the write failed due some other failure
+ */
+fun <T : Any> MongoCollection<T>.updateOneById(
+    id: Any,
+    vararg updates: SetTo<*>,
+    options: UpdateOptions = UpdateOptions()
+): UpdateResult = updateOne(KMongoUtil.idFilterQuery(id), set(*updates), options)
+
+/**
  * Update all documents in the collection according to the specified arguments.
  *
  * @param filter        a document describing the query filter, which may not be null.
