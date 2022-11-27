42184b1e7d0bce13a83db3ce5d62f2f165fd590f
Julien Buret <jburet@gmail.com>
2022-11-27 20:57:06 +0100

    add unit test

---
+++ kmongo-core-tests/src/main/kotlin/org/litote/kmongo/issues/Issue384BehaviourEvaluate.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2016/2022 Litote
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.litote.kmongo.issues
+
+import org.junit.Test
+import org.litote.kmongo.AllCategoriesKMongoBaseTest
+import org.litote.kmongo.evaluate
+import org.litote.kmongo.model.Friend
+
+/**
+ *
+ */
+class Issue384BehaviourEvaluate : AllCategoriesKMongoBaseTest<Friend>() {
+
+    @Test
+    fun `test insert and load`() {
+        col.find().evaluate {
+            println(count() == 0) // prints 'true'
+        }
+    }
+}
